41fb22447dba75c29b7eab561612efcff6fd2466
Samuel O <samuel.olaechea@gmail.com>
2021-11-12 19:06:24 +0000

    Add endpoint to render json from File

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,8 +1,13 @@
 import static ratpack.groovy.Groovy.ratpack
+import java.nio.file.Paths
+
 ratpack {
 	handlers {
 		get {
 			render "Hello World!"
+		}
+		get("json") {
+			render new File(Thread.currentThread().getContextClassLoader().getResource("some.json").toURI()).text
 		}
 		get(":name") {
 			render "Hello $pathTokens.name!"
