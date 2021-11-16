825e9aca4bc43e33a11124bcc333d5da66646343
Samuel <samuel.olaechea@gmail.com>
2021-11-16 12:49:05 -0400

    Read CSV file base directory from JVM arguments.

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,5 +1,11 @@
 import static ratpack.groovy.Groovy.ratpack
+import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+Logger logger = LoggerFactory.getLogger(this.class)
 
 ratpack {
 	handlers {
@@ -8,6 +14,18 @@ ratpack {
 		}
 		get("json") {
 			render new File(Thread.currentThread().getContextClassLoader().getResource("some.json").toURI()).text
+		}
+		get("csv/:name") {
+			response.contentType( "text/csv" )
+
+			String basePath = System.getProperty("ratpackpull_csv_path")
+			Path path = Paths.get(basePath + java.io.File.separator + pathTokens.get("name"))
+
+			if(! Files.exists(path)){
+				logger.error "CSV source file not found at path: " + path
+			}
+
+			render(path)
 		}
 		get(":name") {
 			render "Hello $pathTokens.name!"
